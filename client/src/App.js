ca4eb109981d5a6e863bc2cd4ea14e270cdb8edf
Claudio Antonius <antonius.claudio@gmail.com>
2020-06-08 21:42:18 +0700

    init react

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+const routes = [
+  {
+    exact: true,
+    path: '/',
+    component: Home
+  }
+];
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
